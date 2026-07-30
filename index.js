b7382ab57ae7091dc2af8e4ccfa83340fe34f461
Parking Master <parkingmaster@email.com>
2026-07-30 19:31:10 -0400

    Finish for today

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function openSettings() {
   let content = document.createElement("div");
   content.innerHTML = `
   <h2>Options</h2>
-  <a class="option" href="#" data-disabled>Log out</a>
+  <a class="option" href="#" onclick="this.dataset.disabled = 'disabled', users.logout()" data-disabled>Log out</a>
   <a class="option" href="/account.html" data-disabled>Edit Profile</a>
   <a class="option" href="#">Performance</a>
   <h2>More</h2>
@@ -23,4 +23,32 @@ function openSettings() {
     content: content,
     button: "Close"
   });
+  if (users.loggedIn) document.querySelectorAll(".swal-content a.option").forEach(link => link.removeAttribute("data-disabled"));
+}
+if (users.loggedIn) {
+  document.querySelector(".account .profile-picture").style.backgroundImage = "url(" + users.data.profilePicture + ")";
+  document.querySelector(".account .username").textContent = users.data.username;
+  document.querySelector(".account .points").textContent = users.data.points + " PTS";
+  document.querySelector(".nav-button-login").innerHTML = "Log out";
+  document.querySelector(".nav-button-login").parentElement.href = "#";
+  document.querySelector(".nav-button-login").parentElement.onclick = function() {
+    document.querySelector(".nav-button-login").disabled = "disabled";
+    document.querySelector(".nav-button-login").innerHTML = `<span class="btn-loader"></span>`;
+    users.logout();
+  };
+  document.querySelector(".nav-button-signup").remove();
+  let vehicleNames = {
+    "Honda_Civic": "2019 Honda Civic Sport Touring",
+    "Toyota_RAV4": "2023 Toyota RAV4",
+    "Mini_Cooper": "2014 Mini Cooper S",
+    "Ford_Victoria": "2010 Ford Crown Victoria",
+    "Ford_F150": "2017 Ford F-150 Raptor"
+  };
+  document.querySelector(".current-car-image").src = "/images/cars/" + users.data.currentCar + ".png";
+  document.querySelector(".current-car-text").innerHTML = `<span style="font-weight:600">${vehicleNames[users.data.currentCar].split(" ")[0]}</span> ${vehicleNames[users.data.currentCar].split(" ").splice(1).join(" ").toUpperCase()}</p>`;
+} else {
+  document.querySelector(".account .profile-picture").remove();
+  document.querySelector(".account .username").remove();
+  document.querySelector(".account .points").textContent = (localStorage["points"] || 0) + " PTS";
+  document.querySelector(".account").style = "width: auto; top: 24px; right: 25px";
 }
--- scripts/users.js
+++ scripts/users.js
@@ -1,5 +1,37 @@
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function deleteAllCookies() {
+  let cookies = document.cookie.split(";");
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i];
+    let eqPos = cookie.indexOf("=");
+    let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  }
+}
+
 users = {
   serverURL: "https://parkingmaster-cloud.serveousercontent.com/Parking-Master-5.0",
+  data: null,
+  loggedIn: false,
   login: function(username, password, callback = () => {}) {
     fetch(users.serverURL + "/users/login", {
       method: "POST",
@@ -13,8 +45,11 @@ users = {
     }).then(function(response) {
       if (response.status == 200) {
         response.text().then(sessionKey => {
-          alert(sessionKey);
-          callback(true);
+          setCookie("s", sessionKey);
+          callback(true, 0);
+          users.load(function() {
+            callback(true, 1);
+          });
         });
       } else {
         callback(false, 0);
@@ -60,5 +95,104 @@ users = {
         callback(false);
       }
     });
+  },
+  sendPasswordReset: function(email, callback = () => {}) {
+    fetch(users.serverURL + "/users/reset-password-request", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: email,
+        verificationUrl: location.protocol + "//" + location.host + "/login.html"
+      })
+    }).then(function(response) {
+      if (response.status == 200) {
+        callback(true);
+      } else {
+        callback(false);
+      }
+    });
+  },
+  resetPassword: function(email, verificationKey, newPassword, callback = () => {}) {
+    fetch(users.serverURL + "/users/reset-password", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: email,
+        verificationKey: verificationKey,
+        newPassword: newPassword
+      })
+    }).then(function(response) {
+      if (response.status == 200) {
+        callback(true);
+      } else {
+        callback(false);
+      }
+    });
+  },
+  logout: function() {
+    if (users.loggedIn) {
+      let sessionKey = getCookie("s");
+      fetch(users.serverURL + "/users/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          sessionKey: sessionKey
+        })
+      }).then(function(response) {
+        if (response.status == 200) {
+          deleteAllCookies();
+          localStorage.removeItem("user-cache");
+          location.reload(true);
+        }
+      });
+    }
+  },
+  load: function(callback = () => {}) {
+    let sessionKey = getCookie("s");
+    if (sessionKey) {
+      users.loggedIn = true;
+      if (localStorage["user-cache"]) users.data = JSON.parse(localStorage["user-cache"]);
+      fetch(users.serverURL + "/users/get/" + sessionKey).then(function(response) {
+        if (response.status == 200) {
+          response.json().then(function(data) {
+            users.data = data;
+            localStorage.setItem("user-cache", JSON.stringify(data));
+            callback();
+          });
+        }
+      });
+    }
+  },
+  set: function(key, value) {
+    users.data[key] = value;
+    if (users.loggedIn) {
+      let sessionKey = getCookie("s");
+      fetch(users.serverURL + "/users/set", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          sessionKey: sessionKey,
+          key: key,
+          value: value
+        })
+      }).then(function(response) {
+        if (response.status == 200) {
+          if (localStorage["user-cache"]) {
+            let cache = JSON.parse(localStorage["user-cache"]);
+            cache[key] = value;
+            localStorage.setItem("user-cache", JSON.stringify(cache));
+          }
+        }
+      });
+    }
   }
 };
+users.load();
